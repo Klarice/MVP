1fab8425058cbefd1e7c493a45e501b413e7590a
dbibi <bibid@oa-j318-33.ddi.sheridanc.on.ca>
2014-03-06 16:40:30 -0500

    h3ed it

--- application/views/blog_view.php
+++ application/views/blog_view.php
@@ -1,6 +1,6 @@
     <h1><?php echo $title; ?></h1>
     <?php foreach($posts as $post): ?>
-    	<h2><?php echo $post ['title']; ?></h2>
+    	<h3><?php echo $post ['title']; ?></h3>
         <p><?php echo $post ['content']; ?></p>
         <p><?php echo $post ['categories']; ?></p>
     <?php endforeach; ?>
